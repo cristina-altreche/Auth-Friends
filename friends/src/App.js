6d66373c0a4b71b19f96c5f88ea2cbe6239d9272
Cristina Altreche <c.altreche@gmail.com>
2020-07-22 11:08:47 -0400

    removed reducers and actions

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {Route} from 'react-router-dom'
 import  { Login  } from './components/Login'
 import Navigation from './components/Navigation'
-
 import { FriendsList } from './components/FriendsList'
 import { NewFriend } from './components/NewFriend'
 import PrivateRoute from './utils/PrivateRoute'
